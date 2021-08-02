169aebebe3d917422294f249803d5f9c78bbe994
Simplation <simplationwang@gmail.com>
2021-08-02 11:38:56 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by extra("1.5.21")
     repositories {
         google()
         mavenCentral()
@@ -11,7 +10,10 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        // classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        // 和上面的写法作用一样
+        // classpath("", DependenciesVersion.Gradle)
+        classpath(kotlin("gradle-plugin", DependenciesVersion.Kotlin))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
