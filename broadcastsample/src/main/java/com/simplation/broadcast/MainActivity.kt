2048c67471959c01058c98b16690b8f5532da916
Simplation <simplationwang@gmail.com>
2021-08-09 10:14:31 +0800

    Update network related code

--- broadcastsample/src/main/java/com/simplation/broadcast/MainActivity.kt
+++ broadcastsample/src/main/java/com/simplation/broadcast/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,7 +20,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         intentFilter = IntentFilter()
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
@@ -37,15 +40,36 @@ class MainActivity : AppCompatActivity() {
         unregisterReceiver(networkChangeReceiver)
     }
 
-    class NetworkChangeReceiver() : BroadcastReceiver() {
+    class NetworkChangeReceiver : BroadcastReceiver() {
 
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val connectionManager = context!!.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val networkInfo = connectionManager.activeNetworkInfo
-            if (networkInfo != null && networkInfo.isAvailable) {
-                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show()
-            } else{
-                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show()
+        override fun onReceive(context: Context, intent: Intent) {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT < 23) {
+                val networkInfo = connectivityManager.activeNetworkInfo
+                if (networkInfo != null && networkInfo.isAvailable) {
+                    Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show()
+                    if (networkInfo.type == ConnectivityManager.TYPE_WIFI) {
+                        Toast.makeText(context, "WIFI", Toast.LENGTH_SHORT).show()
+                    } else if (networkInfo.type == ConnectivityManager.TYPE_MOBILE) {
+                        Toast.makeText(context, "移动数据...", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                val network = connectivityManager.activeNetwork
+                val nc = connectivityManager.getNetworkCapabilities(network)
+                if (network != null && nc != null) {
+                    Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show()
+                    if (nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        Toast.makeText(context, "WIFI", Toast.LENGTH_SHORT).show()
+                    } else if (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        Toast.makeText(context, "移动数据...", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
