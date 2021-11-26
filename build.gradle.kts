ac830cf101b74ea9c3ad54143a9f09598e2f9c4c
Simplation <simplationwang@gmail.com>
2021-11-26 17:47:26 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,12 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("com.android.tools.build:gradle:7.0.3")
         // classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         // 和上面的写法作用一样
         // classpath("", DependenciesVersion.Gradle)
         // classpath(kotlin("kotlin-gradle", DependenciesConfig.Kotlin))
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
